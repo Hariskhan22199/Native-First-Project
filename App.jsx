f0cb3837b644e2791335c1933b75ea0b3695313b
Muhammad Haris Khan <muhammadharis3619@gmail.com>
2026-01-02 19:43:32 +0500

    New worrk

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import { Text, View, Button, } from 'react-native'
+import First from './src/components/First'
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 40, fontWeight: 'bold', color: 'blue'}}>React Native App Developer</Text>
+      <Text style={{fontSize: 20, }}>Muhammad Haris Khan</Text>
+      <Button title='press here' onPress={() => alert('Button pressed')} />
+        <First/>
+    </View>
+    
+  )
+}
+
+
+export default App
---
+++ src/components/First.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+const First = () => {
+    return (
+      <View>
+        <Text style={{fontSize: 20, fontWeight: 'bold', color: 'red'}}>First Component</Text>
+      </View>
+    )
+  }
+  export default First
